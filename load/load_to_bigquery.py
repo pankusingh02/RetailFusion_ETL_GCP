f43fe1c890591c26d05a47eaec220e6c645339aa
pankusingh02 <pankajaswal2020@gmail.com>
2025-06-12 22:07:41 +0530

    uploaded to BigQuery and Remove credentials and update gitignore

--- load/load_to_bigquery.py
+++ load/load_to_bigquery.py
@@ -4,21 +4,25 @@ from utils.logger import get_logger
 from google.cloud import bigquery
 from google.oauth2 import service_account
 
-logger= get_logger(__name__)
+logger = get_logger(__name__)
 
 
-def load_to_bigquery(df:pd.DataFrame)-> None:
+def load_to_bigquery(df: pd.DataFrame) -> None:
     try:
         project_id = os.getenv("GCP_PROJECT_ID")
-        dataset = os.getenv("BQ_DATASET","reatail")
-        table = os.getenv("BQ_TABLE","orders_usd")
+        dataset = os.getenv("BQ_DATASET", "reatail")
+        table = os.getenv("BQ_TABLE", "orders_usd")
 
-        full_table_id=f'{project_id}.{dataset}.{table}'
+        full_table_id = f'{project_id}.{dataset}.{table}'
 
         logger.info(f'Loading data to Bigquery table: {full_table_id}')
 
         credentials_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-        credentials = service_account.Credentials.from_service_account_file(credentials_path)
+        if not credentials_path:
+            raise ValueError("GOOGLE_APPLICATION_CREDENTIALS environment variable not set")
+        
+        credentials = service_account.Credentials.from_service_account_file(
+            credentials_path)
 
         # The df.to_gbq() function is a pandas method that writes a DataFrame directly to Google BigQuery
         df.to_gbq(
@@ -29,7 +33,7 @@ def load_to_bigquery(df:pd.DataFrame)-> None:
         )
 
         logger.info(f'Loaded {len(df)} rows to {full_table_id} successfully')
-    
+
     except Exception as e:
         logger.exception("Error while loading the bigQuery")
         raise e
